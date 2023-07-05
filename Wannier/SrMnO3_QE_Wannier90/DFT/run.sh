b1d2b8e547c7b7ad6ec45365381d83fd7d90c95b
lisurui <surui@scape.io>
2023-07-05 13:24:29 +0100

    update to qe 7.2

--- Wannier/SrMnO3_QE_Wannier90/DFT/run.sh
+++ Wannier/SrMnO3_QE_Wannier90/DFT/run.sh
@@ -1,5 +1,5 @@
-mpirun pw.x < SrMnO3.scf.pwi  | tee SrMnO3.scf.pwo
-mpirun pw.x < SrMnO3.nscf.pwi | tee SrMnO3.nscf.pwo
+mpirun pw.x < SrMnO3_72.scf.pwi  | tee SrMnO3.scf.pwo
+mpirun pw.x < SrMnO3_72.nscf.pwi | tee SrMnO3.nscf.pwo
 
 wannier90.x -pp SrMnO3_up
 mpirun pw2wannier90.x < SrMnO3_up.pw2wan | tee SrMnO3_up.pw2wan.out
@@ -10,4 +10,4 @@ wannier90.x -pp SrMnO3_down
 mpirun pw2wannier90.x < SrMnO3_down.pw2wan | tee SrMnO3_down.pw2wan.out
 wannier90.x SrMnO3_down
 
-wann2J.py --posfile SrMnO3.scf.pwi --prefix_up=SrMnO3_up --prefix_down=SrMnO3_down --elements=Mn --efermi=10.67 --kmesh 5 5 5
+wann2J.py --posfile SrMnO3_72.scf.pwi --prefix_up=SrMnO3_up --prefix_down=SrMnO3_down --elements=Mn --efermi=10.67 --kmesh 5 5 5
